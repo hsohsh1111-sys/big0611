6ab36f021153b48225c4a821d75d7a4d715165b5
hsohsh1111-sys <hsohsh1111@gmail.com>
2026-06-19 10:55:17 +0900

    개인사이트 1차 작업완료

---
+++ p01/ch04/ex100_연습.py
@@ -0,0 +1,5 @@
+def greet():
+    print("안녕하세요! 반가워요.")
+
+def greet_person(name):
+    print(f"안녕하세요, {name}님!")    
---
+++ p01/ch05/ex01_함수.py
@@ -0,0 +1,9 @@
+def test():
+    print("함수 연습")
+test()    
+def coffee(temp):
+    if temp > 0 :
+        print("아이스 아메리카노")
+    else:
+        print("따뜻한 아메리카노")
+coffee(30)        
