7869994edb9a9735fdd1126604798e96b539aef5
hsohsh1111-sys <hsohsh1111@gmail.com>
2026-06-22 09:50:52 +0900

    Update ex01_함수.py

--- p01/ch05/ex01_함수.py
+++ p01/ch05/ex01_함수.py
@@ -6,4 +6,25 @@ def coffee(temp):
         print("아이스 아메리카노")
     else:
         print("따뜻한 아메리카노")
-coffee(30)        
+coffee(30)  
+coffee(-10)       
+
+def update_scores(scores) :
+    new_scores = []
+
+    for s in scores:
+        new = s + 5
+        new_scores.append(new)
+    return new_scores
+
+scores = [80, 90, 70, 65, 85, 95, 90, 80, 75, 80]
+
+print(update_scores(scores))
+
+def change_word_case(word):
+    upperCase = word.upper()
+    lowerCase = word.lower()
+    return upperCase, lowerCase
+
+upper, lower =change_word_case('I love Seoul')
+print('대문자는 {1} 이고, 소문자는 {0}이야.'.format(upper, lower))
